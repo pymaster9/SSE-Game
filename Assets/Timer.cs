7b09c262aa8295cd45a027083ce1a52b65b8b393
pymaster9 <adi31tya@icloud.com>
2023-02-26 17:31:07 -0500

    Added a quiz end scene and added some color

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class Timer : MonoBehaviour
 {
     public int maxseconds;
     public static int seconds;
+    public Scene victory;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +20,13 @@ public class Timer : MonoBehaviour
     {
         seconds -= 1;
         GetComponent<TextMeshProUGUI>().text = (seconds/60).ToString()+":"+(seconds%60).ToString("d2");
+        if (seconds < 5)
+        {
+            GetComponent<TextMeshProUGUI>().color = new Color(255, 0, 0);
+        }
+        if (seconds == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
