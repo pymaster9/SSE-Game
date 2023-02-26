2dee537e2ad83ce508ad21ebe58717309d698e8c
pymaster9 <adi31tya@icloud.com>
2023-02-26 17:08:13 -0500

    Got the timer working

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    public int maxseconds;
+    public static int seconds;
     // Start is called before the first frame update
     void Start()
     {
-        
+        seconds = maxseconds + 1;
+        InvokeRepeating("countDown", 0.0f, 1.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void countDown()
     {
-        
+        seconds -= 1;
+        GetComponent<TextMeshProUGUI>().text = (seconds/60).ToString()+":"+(seconds%60).ToString("d2");
     }
 }
